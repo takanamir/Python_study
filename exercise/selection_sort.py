e28095d3442922c56e34ab920d99c6fd987beac7
Takanamiy <rtakanami0706@gmail.com>
2020-03-16 23:36:54 +0900

    選択ソート

---
+++ exercise/selection_sort.py
@@ -0,0 +1,18 @@
+"""
+以下のリストを選択ソートを用いて昇順に並び替える
+list_a = [5,7,4,5,1,2,3,2,9,1,4]
+"""
+
+list_a = [5,7,4,5,1,2,3,2,9,1,4]
+
+# i: 0 => list_aの長さ-1までループしたインデックス
+for i in range(len(list_a)):
+    # min_idx: 1以上のインデックスで最小値の入っているもの
+    min_idx = i
+    # j: i+1 => list_aの長さ-1
+    for j in range(i+1, len(list_a)):
+        if list_a[min_idx] > list_a[j]:
+            min_idx = j
+    list_a[i], list_a[min_idx] = list_a[min_idx], list_a[i]
+
+print(list_a)
