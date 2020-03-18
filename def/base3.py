0dfe0523ecf40cb663d8bc5838b7a16826be32b8
Takanamiy <rtakanami0706@gmail.com>
2020-03-18 22:13:21 +0900

    グローバル変数

---
+++ def/base3.py
@@ -0,0 +1,10 @@
+# グローバル関数
+
+def printAnimal():
+    global animal
+    animal = 'Cat'
+    print('関数内animal = {}, id = {}'.format(animal, id(animal)))
+
+# animal = 'Dog'
+printAnimal()
+print('関数外animal = {}, id = {}'.format(animal, id(animal))) # animalはCat
