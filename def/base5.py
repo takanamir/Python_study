c00b67ae89e6e6a38d23b4a538369c8d54eeff3c
Takanamiy <rtakanami0706@gmail.com>
2020-03-18 22:43:21 +0900

    ジェネレータ関数

---
+++ def/base5.py
@@ -0,0 +1,52 @@
+# ジェネレータ関数
+# def generator(max):
+#     print('ジェネレータ作成')
+#     for n in range(max): # n:0～9
+#         yield n
+#         print('yield実行')
+
+def generator(max):
+    print('ジェネレータ作成')
+    for n in range(max): # n:0～9
+        try:
+            x = yield n
+            print('x = {}'.format(x))
+            print('yield実行')
+        except ValueError as e:
+            print('throwを実行しました')
+
+gen = generator(10)
+next(gen) # xはNone
+gen.send(100) # xは100
+gen.close() # StopIterationする
+next(gen)
+# gen.throw(ValueError('Invalid Value'))
+# n = next(gen) # ジェネレータ作成
+# print('n = {}'.format(n)) # n = 0
+# n = next(gen) # yield実行
+# print('n = {}'.format(n)) # n = 1
+
+# for x in gen:
+#     print('x = {}'.format(x))
+"""
+x = 0
+yield実行
+x = 1
+yield実行
+x = 2
+yield実行
+x = 3
+yield実行
+x = 4
+yield実行
+x = 5
+yield実行
+x = 6
+yield実行
+x = 7
+yield実行
+x = 8
+yield実行
+x = 9
+yield実行
+"""
